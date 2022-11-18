0afd995c73b23f197c24f21dd2da92ed79813641
OscarLugo60 <oscarlugo60@gmail.com>
2022-11-18 10:12:56 -0400

    First commit

---
+++ dolar.py
@@ -0,0 +1,33 @@
+import requests
+import re
+from datetime import datetime
+import psycopg2
+
+url = "https://monitordolarvenezuela.com/historico/bcv-banco-central-de-venezuela"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    content = response.text
+    regex = """<div id="Costo">
+(.+?) Bs
+</div>"""
+
+    for precio in re.findall(regex, content):
+        dolar = float(precio.replace(',','.'))
+
+hoy = datetime.today()
+
+conexion = psycopg2.connect(
+    database="labdb",
+    user="oscar",
+    password="oscar1234"
+)
+
+cursor1=conexion.cursor()
+
+sql = "insert into factura_dolar(created,modified,valor) values (%s,%s,%s)"
+datos = (hoy,hoy,dolar)
+cursor1.execute(sql,datos)
+conexion.commit()
+conexion.close()
